074c71c49dcb7020b81bdae6cc67fa5f0502288b
amagalhaes <alexandremagalhaes31@gmail.com>
2020-11-28 15:35:32 -0300

    Implement the Save (CRUD) for indexDB

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+import HtmlService from "./HtmlService.js";
+import TodoService from "./TodoService.js";
+
+class App {
+  constructor() {
+    this.registerServiceWorker();
+    this.initialize();
+  }
+  
+  initialize() {
+    new HtmlService(new TodoService());
+  }
+
+  registerServiceWorker() {
+    if ("serviceWorker" in navigator) {
+      const onsuccess = () => console.log("[Service Worker] Registered");
+      const onfailure = () => console.log("[Service Worker] Failed");
+
+      navigator.serviceWorker
+        .register("sw.js")
+        .then(onsuccess)
+        .catch(onfailure);
+    }
+  }
+}
+
+new App();
+
